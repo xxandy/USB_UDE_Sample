5514029c2178b7edb0c9e6efdd9b6088c414ad12
Alex Gimenez <frommar5@hotmail.com>
2018-03-24 15:43:00 -0700

    removed crashing de-init`

--- UDEFX2/Device.c
+++ UDEFX2/Device.c
@@ -513,15 +513,18 @@ ControllerWdfEvtCleanupCallback(
 	WDFOBJECT   WdfDevice
 )
 {
-	PIO_CONTEXT ioContext;
+	//PIO_CONTEXT ioContext;
+	UNREFERENCED_PARAMETER(WdfDevice);
 	FuncEntry(TRACE_DEVICE);
 
+	/*
 	ioContext = WdfDeviceGetIoContext((WDFDEVICE)WdfDevice);
 
 	WdfSpinLockAcquire(ioContext->InProgressLock);
 	MbbCleanupBufferQueue(&ioContext->AvailableInterrupt);
 	MbbCleanupBufferQueue(&ioContext->EncapsulatedCommandResponse);
 	WdfSpinLockRelease(ioContext->InProgressLock);
+	*/
 
 	// TNEXT
 	// Usb_Destroy((WDFDEVICE)WdfDevice);
