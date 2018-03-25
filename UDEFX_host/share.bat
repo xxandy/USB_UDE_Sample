27796d74ba13de3fee9adc43fbe0ad7134f111f0
Alex Gimenez <frommar5@hotmail.com>
2018-03-25 11:30:56 -0700

    small trace, added host side as copy from real FX2

---
+++ UDEFX_host/share.bat
@@ -0,0 +1,6 @@
+@setlocal
+copy /y exe\x64\Debug\osrusbfx2.exe %userprofile%\vmshare\usbt
+copy /y driver\x64\Debug\*.sys %userprofile%\vmshare\usbt
+copy /y driver\x64\Debug\*.inf %userprofile%\vmshare\usbt
+copy /y driver\x64\Debug\*.cat %userprofile%\vmshare\usbt
+
