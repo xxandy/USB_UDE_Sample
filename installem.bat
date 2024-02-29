1b7b9ec863658603eefd6aa40e70aaf2f8508b73
Fredrik Orderud <fredrik.orderud@ge.com>
2024-02-29 14:47:25 +0100

    Install driver certificate before installing drivers
    Changes:
    * Change hostude copying destination to $(PackageDir), so that the drivers files end up in a separate folder.
    * Also copy certificates to $(PackageDir) so that all required files are in the same folder.
    * Install driver certificate to get rid of "Windows can't verify the publisher of this driver software" warning dialog when afterwards installing the drivers.

--- installem.bat
+++ installem.bat
@@ -1,3 +1,7 @@
+:: Trust driver certificate (the hostude cert suffice, since UDEFX2 is signed with the same cert)
+certutil.exe -addstore root hostude.cer
+certutil.exe -addstore trustedpublisher hostude.cer
+
 devcon.exe install hostude.inf "USB\VID_1209&PID_0887"
 
 devcon.exe install UDEFX2.inf Root\UDEFX2
