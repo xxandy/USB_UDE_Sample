cc8960a265f85765e9e3acc40053387ed2062f02
Fredrik Orderud <fredrik.orderud@ge.com>
2024-03-27 10:00:17 +0100

    Delete UDEFX2\share.bat script that is now unreferenced after share.bat was deleted.

--- UDEFX2/share.bat
+++
@@ -1,7 +0,0 @@
-@setlocal
-mkdir %userprofile%\vmshare\usbt >nul 2>&1
-copy /y x64\Debug\UDEFX2.sys %userprofile%\vmshare\usbt
-copy /y x64\Debug\UDEFX2.inf %userprofile%\vmshare\usbt
-copy /y x64\Debug\UDEFX2\udefx2.cat %userprofile%\vmshare\usbt
-copy /y trace\*.bat %userprofile%\vmshare\usbt
-
