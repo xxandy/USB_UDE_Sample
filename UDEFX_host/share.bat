b0c3242e8616d8409c44d47f7a25889b9a4a6ea7
Fredrik Orderud <fredrik.orderud@ge.com>
2024-03-29 15:00:41 +0100

    Remove line that refers to the deleted driver_both.txt file.

--- UDEFX_host/share.bat
+++ UDEFX_host/share.bat
@@ -1,6 +1,5 @@
 @setlocal
 mkdir %userprofile%\vmshare\usbt >nul 2>&1
-copy /y driver_both.txt %userprofile%\vmshare\usbt
 copy /y exe\x64\Debug\hostudetest.exe %userprofile%\vmshare\usbt
 copy /y driver\x64\Debug\*.sys %userprofile%\vmshare\usbt
 copy /y driver\x64\Debug\*.inf %userprofile%\vmshare\usbt
